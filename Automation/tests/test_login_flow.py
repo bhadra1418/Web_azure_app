1b5a59ae2af4eddb1d9128fb52907f9355a747f2
bhadra1418 <vbhadra06@gmail.com>
2025-08-02 14:33:01 +0530

    end to end test case added and web app updated

--- Automation/tests/test_login_flow.py
+++ Automation/tests/test_login_flow.py
@@ -8,7 +8,7 @@ def test_login_flow():
     driver.get("http://127.0.0.1:5000/login")
 
     # login
-    driver.find_element(By.NAME, "email").send_keys("demo@gmail.com")
+    driver.find_element(By.NAME, "email").send_keys("demo1@gmail.com")
     driver.find_element(By.NAME, "password").send_keys("demo")
     driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
 
